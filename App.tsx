071a3bb0e8b04ac92f224feaab9cb238ab170bea
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-05-22 00:33:25 +0900

    readFile

--- App.tsx
+++ App.tsx
@@ -4,8 +4,9 @@
  *
  * @format
  */
+import * as RNFS from 'react-native-fs';
 import RecordScreen, {RecordingStartResponse} from 'react-native-record-screen';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -14,11 +15,15 @@ import {
   Text,
   useColorScheme,
   View,
+  PermissionsAndroid,
+  Button,
 } from 'react-native';
 
 function App(): JSX.Element {
+  const [content, setContent] = useState(null);
+
   useEffect(() => {
-    startRecording();
+    // startRecording();
   }, []);
 
   const startRecording = async () => {
@@ -37,11 +42,57 @@ function App(): JSX.Element {
     }, 3000);
   };
 
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        {
+          title: 'Cool Photo App Camera Permission',
+          message:
+            'Cool Photo App needs access to your camera ' +
+            'so you can take awesome pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the camera');
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const readFile = () => {
+    RNFS.readDir(RNFS.DocumentDirectoryPath)
+      .then(result => {
+        console.log('GOT RESULT', result);
+        return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+      })
+      .then(statResult => {
+        if (statResult[0].isFile()) {
+          return RNFS.readFile(statResult[1], 'utf8');
+        }
+        return 'no file';
+      })
+      .then(contents => {
+        setContent(contents);
+        console.log(contents);
+      })
+      .catch(err => {
+        console.log(err.message, err.code);
+      });
+  };
+
   return (
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
           <Text>Hello</Text>
+          <Button title="request permissions" onPress={readFile} />
         </View>
       </ScrollView>
     </SafeAreaView>
